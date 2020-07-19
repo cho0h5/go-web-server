72ba6a838889e47063fbab75ee3aa36c2b6397fa
Youngho Cho <dominoyh5@gmail.com>
2020-07-19 17:16:25 +0900

    Make main say hello

--- main.go
+++ main.go
@@ -2,8 +2,32 @@ package main
 
 import (
     "fmt"
+    "net"
+    "log"
 )
 
 func main() {
-    fmt.Println("Hello, world!");
+    fmt.Println("Start Server");
+
+    l, err := net.Listen("tcp", ":8080")
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer l.Close()
+
+    for {
+        conn, err := l.Accept()
+        if err != nil {
+          log.Println(err)
+          continue
+        }
+        go handler(conn)
+    }
+}
+
+func handler(conn net.Conn) {
+    data := "HTTP/1.1 200 OK\n\nhello"
+    conn.Write([]byte(data))
+
+    conn.Close()
 }
